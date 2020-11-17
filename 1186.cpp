cb41ddbc9537c4254840c88163847721b51318a0
kapike <kapike@qq.com>
2020-11-17 19:03:20 +0800

    chang short to long long and chang array to two integer

--- 1186.cpp
+++ 1186.cpp
@@ -7,31 +7,35 @@ int mymax(int a, int b, int c){
 }
 
 int main(){
-  short t;
-  vector<short> data;
-  while(scanf("%hd",&t)!=EOF){
-    data.push_back(t);
-  }
-  int n = data.size();
-  short f1[n];
-  short f2[n];
-  short smax[n];
-  
-  f1[0] = data[0];
-  f2[0] = data[0];
-  smax[0] = data[0];
-  short maxresult = data[0];
-  
-  int tmp ;
-  for(int i=1; i<n; i++){
-    smax[i] = smax[i-1]>0 ? smax[i-1]+data[i] : data[i];
-    f1[i] = mymax(data[i]+f2[i-1],smax[i],smax[i]);
-    f2[i] = mymax(data[i]+f2[i-1],smax[i],smax[i-1]);
-    if(maxresult < f1[i]){
-      maxresult = f1[i];  
+  long long  t;
+  int i = 0;
+  long long maxres;
+  long long lastsmax;
+  long long thissmax;
+  long long lastf1;
+  long long thisf1;
+  long long lastf2;
+  long long thisf2;
+  while(scanf("%lld",&t)!=EOF){
+    if(i==0){
+      thisf1 = t;
+      thisf2 = t;
+      thissmax = t;
+      maxres = t;
+    }else{
+      lastf1 = thisf1;
+      lastf2 = thisf2;
+      lastsmax = thissmax;
+      thissmax = lastsmax>0 ? lastsmax+t : t;
+      thisf1 = mymax(t+lastf2,thissmax,thissmax);
+      thisf2 = mymax(t+lastf2, lastsmax,thissmax);
+      if(thisf1 > maxres){
+        maxres = thisf1;
+      } 
     }
+    i++;
   }
-  printf("%d\n",maxresult);
+  printf("%lld\n",maxres);
   return 0;
 }
 
