325b362f5f39e0fc4e757c923c8759f4e650c3b9
kapike <kapike@qq.com>
2020-11-17 15:43:24 +0800

    add 403.cpp

---
+++ 403.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<set>
+#include<map>
+using namespace std;
+int main(){
+
+  int n ;
+  unsigned  *data;
+  scanf("%d",&n);
+  data = new unsigned[n]; 
+  map<unsigned, set<unsigned> > decison;
+  
+
+  unsigned step;
+  for(int i=0;i<n;i++){
+    scanf("%u",&step);
+    data[i] = step;
+    decison[step] = set<unsigned>(); 
+  }
+  decison[data[1]].insert(1);
+  
+  for(int s = 0; s<n-1; s++){
+    if(decison[data[s]].size()!=0){
+       for(auto it : decison[data[s]]){
+         for(int i =-1; i<2; i++)
+           if(it+i<=data[n-1] && it+i>=data[0]) 
+            decison[it+i].insert(it+i);
+       }
+    }
+  }
+  if(decison[data[n-1]].size()!=0){
+    printf("true\n");
+  }else{
+    printf("false\n");
+  }
+
+  return 0;
+}
