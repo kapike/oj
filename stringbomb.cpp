7129686e7b50e6152a6bb6c3587334261a14fb37
kapike <kapike@qq.com>
2020-12-04 21:03:12 +0800

    greedy oj first problem

---
+++ stringbomb.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+  char first;
+  char second;
+  char third;
+  int count = 0;
+  while(scanf("%c",&first)!=EOF){
+    
+    if(count == 0){
+      count++;
+    }else if(first == 'B'){
+      count--;
+    }else if(first == 'A'){
+      count++;
+    }
+    //printf("%c %d %d\n",first,first,count);
+  }
+
+  printf("%d\n",count);
+  return 0;
+}
